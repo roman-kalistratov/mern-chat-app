bf78056924e0d21ff58f4f026380a269cf20ece6
Roman Kalistratov <80212286+roman-kalistratov@users.noreply.github.com>
2024-04-17 21:01:30 +0300

    netlify serverURI update

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,5 @@
-const serverURI = "http://localhost:5000";
+// const serverURI = "http://localhost:5000";
+const serverURI =
+  "https://66200e1484e7b60008d99b96--mern-chatapp-2024.netlify.app/";
 // const serverURI = "https://mern-chat-app-s9eg.onrender.com";
 export default serverURI;
