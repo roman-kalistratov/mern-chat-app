a35a419af6e6535ab743fc490613919e74be84db
Roman Kalistratov <80212286+roman-kalistratov@users.noreply.github.com>
2024-04-21 18:13:38 +0300

    serverURI updated

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,5 @@
 // const serverURI = "http://localhost:5000";
 
-const serverURI = "https://mern-chat-app-s9eg.onrender.com";
+// const serverURI = "https://mern-chat-app-s9eg.onrender.com";
+const serverURI = "https://mern-chat-app-flame-phi.vercel.app";
 export default serverURI;
